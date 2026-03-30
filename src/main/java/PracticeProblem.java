24f28bc8847004a5c5e582325c1b09aec8ba33f2
Akshayen Jeyamaran <akshayen.jeyamaran@student.tdsb.on.ca>
2026-03-30 14:02:28 +0000

    Finish Questions 1 and 2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,20 @@
 public class PracticeProblem {
-
 	public static void main(String args[]) {
-
 	}
-
-	
-
+	public static int sumOfDigits(int x) {
+		x = Math.abs(x);
+	    if (x == 0 && x <= 9) {
+	      return 0;
+	    }
+		return (x%10) + sumOfDigits(x / 10); 
+	}
+	public static int factorial(int y) {
+	    if (y == 0) {
+	      return 1;
+	    }
+		if (y < 0) {
+	      return 0;
+	    }
+		return y * factorial(y-1); 
+	}
 }
